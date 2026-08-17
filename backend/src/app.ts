757fe59d53e7706f6a76ab4a2fc21cd9b032114b
Emilia <emiliama2006@gmail.com>
2026-08-17 00:10:28 -0400

    authentication start

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -11,9 +11,15 @@ import express from "express";
 import cors from "cors";
 import {apiRouter} from "./routes/index.js";
 
+import {toNodeHandler} from "better-auth/node";
+import {auth} from "./auth.js";
+
 export const app = express();
 app.set("trust proxy", 1);
 
-app.use(cors());
+
+app.use(cors({origin: "http://localhost:5173", credentials: true}));
+app.all("/api/auth/*splat", toNodeHandler(auth));
+
 app.use(express.json());
 app.use("/api", apiRouter);
